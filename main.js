66c50faa79c99ea221511cb36def479997ed1315
Jessica Leivers <sarahjessicaleivers.com>
2015-04-06 17:38:39 -0700

    done

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 var a = prompt('give me a num?');
 a = parseInt(a);
+var operator = prompt("give me an operator");
 var b = prompt('give me a another num?');
 b = parseInt(b);
-var c = Math.pow(a,b);
-console.log('a', a, 'b', b, 'c', c);
+var answer = 0;
+
+if (operator === "*") {answer = a * b; }
+else if (operator === "/") {answer = a / b; }
+else if (operator === "-") {answer = a - b; }
+else {answer = a + b; }
+
+alert("the answer is " + answer);
+
+
+//var c = Math.pow(a,b);
+//console.log('a', a, 'b', b, 'c', c);
