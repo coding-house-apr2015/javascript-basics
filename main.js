b936b4727f7cf70081469194d58e8924a5437c0d
Dick <rljarm@gmail.com>
2015-04-07 12:51:22 -0700

    solved problem 2

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
-var a = prompt('Enter the first number.');
-a = parseFloat(a);
-var o = prompt('Enter an arithmetic operator ( + , - , * or / ).');
-var c = prompt('Please enter the second number.');
-c = parseFloat(c);
-var result;
-switch (o) {
-    case '+':
-      result = a + c;
-      break;
-    case '-':
-      result = a - c;
-      break;
-    case '*':
-      result = a * c;
-      break;
-    case '/':
-      result = a / c;
-      break;
+// var a = prompt('Enter the first number.');
+// a = parseFloat(a);
+// var o = prompt('Enter an arithmetic operator ( + , - , * or / ).');
+// var c = prompt('Please enter the second number.');
+// c = parseFloat(c);
+// var result;
+// switch (o) {
+//     case '+':
+//       result = a + c;
+//       break;
+//     case '-':
+//       result = a - c;
+//       break;
+//     case '*':
+//       result = a * c;
+//       break;
+//     case '/':
+//       result = a / c;
+//       break;
+// }
+// console.log('The result of a', o, 'b is', result);
+
+var a = prompt("Give me numbers seperated by a :");
+
+nums = a.split(':');
+
+nums = nums.map(function(n){
+  return parseFloat(n);
+});
+
+var sum = 0;
+	for(var i = 0; i < nums.length; i++){
+	    sum += nums[i];
 }
-console.log('The result of a', o, 'b is', result);
+
+var avg = sum/nums.length;
+
+var product = 1;
+for(var i = 0; i < nums.length; i++){
+    product *= nums[i];
+    console.log(nums[i]);
+}
+
+nums = nums.map(function(n){return Math.pow((n - avg),2);});
+var variance = sum / nums.length;
+var stanDev = Math.sqrt(variance);
+
+var variance = (nums.reduce(function(a,b){return a + b;}) / nums.length);
+alert("Your sum is " + sum + " your product is " + product + " your average is "+ avg + " and your standard deviation is " + stanDev + ".");
