914864a4faaeb4552aac7e878b93e40ef3f4e1fc
Clyde G Marl <marl.clyde@gmail.com>
2015-04-07 11:58:33 -0700

    solution for problem 2

--- main.js
+++ main.js
@@ -22,3 +22,24 @@
 // console.log('The result of a', o, 'b is', result);
 
 // problem 2 : feature
+
+/* Finds standard deviation of given numbers
+accepts number as string , each separated by ':'*/
+
+var string = prompt("Enter string of colon separated numbers:");
+var nums = string.split(':');
+var avg = 0;
+
+nums = nums.map(function(char){
+  avg += parseFloat(char);
+  return parseFloat(char);
+});
+
+avg = (avg / nums.length);
+
+nums = nums.map(function(n){
+  return Math.pow((n - avg),2);
+});
+
+var variance = (nums.reduce(function(a,b){return a + b;}) / nums.length);
+window.alert(Math.sqrt(variance));
