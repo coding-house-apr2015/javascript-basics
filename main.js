6a42830ac09c2343ba47bb7c6f4a26a727807218
Sheryl Boughton <sherpbo@gmail.com>
2015-04-06 17:39:23 -0700

    problem 1

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ var b = prompt('give me a another num?');
 b = parseInt(b);
 var c = Math.pow(a,b);
 console.log('a', a, 'b', b, 'c', c);
+
+var a = prompt('give me a num?');
+a = parseInt(a);
+var b = prompt('give me a another num?');
+b = parseInt(b);
+var c = prompt("give me an operator; +, -, *, /");
+if (c == "+"){
+ console.log(a+b)}
+else if (c == "-"){
+  console.log(a-b);
+}
+else if (c == "/"){
+  console.log(a/b);
+}
+else {
+  console.log(a*b);
+}
