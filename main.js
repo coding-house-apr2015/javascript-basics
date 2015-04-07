580e90981d6ec2e38faf4716074eff52873e3178
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-07 12:29:03 -0700

    solve problem 2

--- main.js
+++ main.js
@@ -22,3 +22,18 @@
 // console.log('The result of a', o, 'b is', result);
 
 // problem 2 : feature
+var str = prompt('Enter a string of numbers separated by ":" ');
+var strArr = str.split(':');
+var numArray = strArr.map(parseFloat);
+var sum = 0;
+numArray.forEach(function(x){sum += x;});
+var product = numArray.reduce(function(previousValue, currentValue){
+  return previousValue * currentValue;
+});
+var average = sum / numArray.length;
+var squareSum = numArray.reduce(function(prev, curr){return prev + Math.pow(curr - average, 2);});
+var stdev = Math.sqrt(squareSum / numArray.length);
+console.log('Your sum is: ', sum);
+console.log('Your product is: ', product);
+console.log('Your average is: ', average);
+console.log('Your standard deviation is: ', stdev);
