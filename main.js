b5d167a7e2bbb57da242ccce3320684716369b41
Christian Richard <lifepupil@gmail.com>
2015-04-06 17:58:15 -0700

    solution to prob1

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-var a = prompt('give me a num?');
-a = parseInt(a);
-var b = prompt('give me a another num?');
-b = parseInt(b);
-var c = Math.pow(a,b);
-console.log('a', a, 'b', b, 'c', c);
+var n1 = prompt('give me a num');
+n1 = parseInt(n1);
+
+var op = prompt('give me an operator (*, /, +, -)');
+
+var n2 = prompt('give me a another num?');
+n2 = parseInt(n2);
+
+var ans;
+
+if (op=="*") {ans = n1*n2};
+if (op=="/") {ans = n1/n2};
+if (op=="+") {ans = n1+n2};
+if (op=="-") {ans = n1-n2};
+// var ans = eval(n1+op+n2;)
+
+
+console.log("the result of ", n1, " ", op, " " , n2, " is ", ans, ".");
