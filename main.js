420ecca2c5722f3037d0f41bdd1ae79b4517c0d5
ojangali <niroumandzadeh@gmail.com>
2015-04-06 19:03:29 -0700

    Omid Javascript Basics

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-alert('hey');
+
+// Omid N.'s in-class math program
+var a = prompt("Please pick first number: ");
+var b = prompt("Please pick second number: ");
+
+var c = prompt("Please pick operator (*,+,-,/,%): ");
+
+if (c === "+"){
+    console.log(a + b);
+}
+else if (c === "-"){
+    console.log(a-b);
+}
+else if (c === "*"){
+    console.log(a*b);
+}
+else if (c === "/"){
+    console.log(a/b);
+}
+else if (c === "%"){
+    console.log(a%b);
+}
