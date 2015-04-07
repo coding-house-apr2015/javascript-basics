902d30194f8a65f253e3b0a2eefdb2e412474e34
Chris Oyler <christopher.oyler@gmail.com>
2015-04-06 17:48:01 -0700

    terrible solution

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-var a = prompt('give me a num?');
+/*var a = prompt('give me a num?');
 a = parseInt(a);
 var b = prompt('give me a another num?');
 b = parseInt(b);
 var c = Math.pow(a,b);
 console.log('a', a, 'b', b, 'c', c);
+*/
+
+var a = prompt('Give me a number, please');
+a = parseInt(a);
+var b = prompt('Would you like to add, subtract, multiply, or divide?');
+if (b !== '+' || '-' || '*' || '/') {
+  alert("I'm afraid I can't do that Dave");
+}
+var c = prompt('Another number please');
+c = parseInt(c);
+if (b === '*') {
+  console.log(a * c);
+}
+else if (b === "-") {
+  console.log(a - c);
+}
+else if (b === '+') {
+  console.log(a + c);
+}
+else {
+  console.log(a / c);
+}
