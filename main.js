0baaef3badb47f1cf346a3efd442591e4e8ceb5f
Christian Richard <lifepupil@gmail.com>
2015-04-07 13:35:19 -0700

    2nd commit

--- main.js
+++ main.js
@@ -23,14 +23,31 @@
 
 // problem 2 : feature
 
-var raw = prompt('Give me three numbers separated by ':'');
-var raw = "5:6:7"
+var raw = prompt("Give me three numbers separated by ':'");
+// var raw = "5:6:7"
 var numstr = raw.split(':');
 var numarr = numstr.map(function(num) {
   return parseFloat(num);
 });
 
 var sum = 0;
-var product = 0;
+var product = 1;
 var avg = 0;
+var variance = 0;
 var stdev = 0;
+
+for (var i = 0; i<numarr.length; i++) {
+  sum += numarr[i];
+  product *= numarr[i];
+}
+
+
+var n = numarr.length;
+avg = sum/n;
+
+for (var i = 0; i<numarr.length ; i++) {
+  variance += Math.pow((numarr[i]-avg),2);
+}
+stdev =Math.sqrt(variance/n);
+
+console.log("sum",sum,"product",product,"avg",avg,"standard deviation",stdev);
