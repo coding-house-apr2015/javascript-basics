7097136c053cadad15bcbceedb6e847a23722f1a
Christian Richard <lifepupil@gmail.com>
2015-04-06 23:32:56 -0700

    streamlining code

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-var n1 = prompt('give me a num');
+
+n1 = prompt('give me a num');
 n1 = parseInt(n1);
-
 var op = prompt('give me an operator (*, /, +, -)');
-
 var n2 = prompt('give me a another num?');
 n2 = parseInt(n2);
 
-var ans;
+var ans = "";
 
-if (op=="*") {ans = n1*n2};
-if (op=="/") {ans = n1/n2};
-if (op=="+") {ans = n1+n2};
-if (op=="-") {ans = n1-n2};
-// var ans = eval(n1+op+n2;)
-
-
-console.log("the result of ", n1, " ", op, " " , n2, " is ", ans, ".");
+if (op==="*") {
+    ans = n1*n2;
+  }
+else if (op==="/") {
+    ans = n1/n2;
+  }
+else if (op==="+") {
+    ans = n1+n2;
+  }
+else if (op==="-") {
+    ans = n1-n2;
+  }
+// else if {
+//   console.log("You've entered values that are not permitted.")
+ // else {
+  console.log("the result of", n1, op, n2, "is", ans);
+// }
